222613c0cf47d5b858180eb7c32851b0e1524eba
Tim Dohmen <dohmentim@yahoo.com>
2019-10-07 09:26:48 -0600

    init

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace morningChallenge
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+
+      int doubleInteger(int num)
+      {
+        int sol = num + num;
+        return sol;
+      }
+
+      System.Console.WriteLine("Part One");
+      string choice = Console.ReadLine().ToLower();
+
+      if (int.TryParse(choice, out int index))
+      {
+
+        System.Console.WriteLine(doubleInteger(index));
+      }
+
+    }
+  }
+}
